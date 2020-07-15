d2542b8027ae8bf70416d72a1c00174460389257
April Tran <april@parsleyhealth.com>
2020-07-15 17:24:28 -0400

    update fish config & font

--- fish/config.fish
+++ fish/config.fish
@@ -5,15 +5,15 @@ if not functions -q fisher
 end
 
 set fish_greeting
-set -g theme_short_path yes
+set theme_short_path yes
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/arpil/Code/google-cloud-sdk/path.fish.inc' ]; . '/Users/arpil/Code/google-cloud-sdk/path.fish.inc'; end
 
 # aliases
 # general
-alias vz="vim ~/.zshrc"
-alias sz="source ~/.zshrc"
+alias vz="vim ~/code/config/fish/config.fish"
+alias sz="source ~/code/config/fish/config.fish"
 
 # git
 alias gaa="git add -A"
@@ -34,24 +34,29 @@ alias gpup="git push --set-upstream origin"
 alias gstat="git status"
 alias gunstage="git reset"
 alias guncommit="git reset HEAD^"
+function ch
+  git checkout -b ch$argv
+end
 function gpresquash
   git fetch
   git reset (git merge-base master $argv)
 end
 
 # dr parsley
-alias dr="cd ~/code/dr-parsley"
-alias drs="dr && npm start"
-alias drstartall="brew services start nginx && dr && npm start"
-alias apo="npm run graphql:setup"
-alias sto="npm run storybook"
-alias testd="npm run test:debug"
-alias tests="npm run test --silent"
+alias dr="cd ~/code/dr"
+alias drs="dr && yarn start"
+alias drstartall="brew services start nginx && drs"
+alias apo="yarn get:graphql"
+alias sto="yarn storybook"
+alias testd="yarn test:debug"
+alias tests="yarn test --silent"
 
 # parsley apis
-alias dapiprod="cd ~ && ./cloud_sql_proxy -instances=parsley-production-1:us-east1:sage-database=tcp:5432"
-alias dapistag="cd ~ && ./cloud_sql_proxy -instances=parsley-staging-1:us-east1:sage-database=tcp:5435"
+alias dapiprod="cd ~/code && ./cloud_sql_proxy -instances=parsley-production-1:us-east1:sage-database=tcp:5432"
+alias dapistag="cd ~/code && ./cloud_sql_proxy -instances=parsley-staging-1:us-east1:sage-database=tcp:5435"
 alias dapifwd="kubectl port-forward deployment/data-api-server 8000"
+alias ppprod="cd ~/code && ./cloud_sql_proxy -instances=parsley-production-1:us-east1:peppermint-database=tcp:5438"
+alias ppstag="cd ~/code && ./cloud_sql_proxy -instances=parsley-staging-1:us-east1:peppermint-database=tcp:5437"
 
 alias nginxs="brew services start nginx"
 alias nginxk="brew services stop nginx"
